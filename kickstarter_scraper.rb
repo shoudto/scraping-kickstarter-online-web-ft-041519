bdb1ae17a52c388765d1271c590c472021ed2b2f
Oscar Shoudt <shoudto@gmail.com>
2019-05-14 20:48:51 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,7 +8,7 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   
-  create_project = {}
+  projects = {}
   
    kickstarter.css("li.project.grid_4").each do |project|
     projects[project] = {}
