cc3441b940615b8a0cde8fe0e02dbcf4ffc615fe
Oscar Shoudt <shoudto@gmail.com>
2019-05-14 20:38:33 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,9 +3,9 @@
 require "nokogiri"
 
 def create_project_hash
+  
   create_project = {}
   
   html = File.read('fixtures/kickstarter.html')
- 
-kickstarter = Nokogiri::HTML(html)
+  kickstarter = Nokogiri::HTML(html)
 end
