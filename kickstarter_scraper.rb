c7505560bd9cd6d2c482ced037a0f9c43e6d8a2b
Oscar Shoudt <shoudto@gmail.com>
2019-05-14 20:43:29 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,3 +11,7 @@ def create_project_hash
 end
 
 # projects: kickstarter.css("li.project.grid_4")
+
+# projects: kickstarter.css("li.project.grid_4")
+
+# title: project.css("h2.bbcard_name strong a").text
